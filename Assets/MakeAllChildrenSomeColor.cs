19787a6a6a87890a38ca5b18c0e9fc1227abe256
Eli Feasley <eliana@khanacademy.org>
2016-02-26 03:49:50 -0800

    make it so a bunch of colors can be set at once

---
+++ Assets/MakeAllChildrenSomeColor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeAllChildrenSomeColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		foreach (Renderer child in GetComponentsInChildren(typeof(Renderer))) {
+			child.material.color = new Color (1, 0, 0);
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
